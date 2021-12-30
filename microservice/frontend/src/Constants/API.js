74c1e363bcbc098d1025d04c29004575fa7c09b7
Utkarsh Singh <30589139+Utkarsh1999@users.noreply.github.com>
2021-12-30 12:17:43 +0530

    removed error from react-app

--- microservice/frontend/src/Constants/API.js
+++ microservice/frontend/src/Constants/API.js
@@ -2,9 +2,9 @@ export const BASE_URL = "http:/";
 // export const ACCOUNT_BASE_URL = "/api-account:8080";
 // export const CATALOGUE_BASE_URL = "/api-catalog:8080";
 // export const ORDER_BASE_URL = "/api-order:8080";
-const ACCOUNT_BASE_URL = "/52.167.202.59:8081";
-const CATALOGUE_BASE_URL = "/52.167.202.59:8082";
-const ORDER_BASE_URL = "/52.167.202.59:8083";
+let ACCOUNT_BASE_URL = "/52.167.202.59:8081";
+let CATALOGUE_BASE_URL = "/52.167.202.59:8082";
+let ORDER_BASE_URL = "/52.167.202.59:8083";
 if (process.env.REACT_APP_BASE_IP !== undefined) {
   ACCOUNT_BASE_URL = `/${process.env.REACT_APP_BASE_IP}:${process.env.REACT_APP_ACCOUNT_PORT}`;
   CATALOGUE_BASE_URL = `/${process.env.REACT_APP_BASE_IP}:${process.env.REACT_APP_CATALOGUE_PORT}`;
